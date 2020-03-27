c0c79c278ca551b0377aca89bb09ee7fa9437fdc
yourcoke <deinkoks@gmail.com>
2020-03-27 10:02:05 +0100

    added markdown output

--- src/souper.py
+++ src/souper.py
@@ -3,16 +3,21 @@
 import requests
 from functools import reduce
 from bs4 import BeautifulSoup
+import html2markdown
 
 URL = 'https://www.uni-due.de/med/corona'
-path = 'content.html'
+basepath = 'content'
 
 page = requests.get(URL)
 soup = BeautifulSoup(page.text, 'html.parser')
 jumbos = soup.find_all(class_='jumbotron')
 
 
-with open(path,'w+') as file:
+with open(basepath+'.html','w+') as file:
     content = reduce(lambda x,y: x+y,[x.contents for x in jumbos])
     content = [str(x) for x in content]
+    file.writelines(content)
+
+with open(basepath+'.md','w+') as file:
+    content = [html2markdown.convert(x) for x in content]
     file.writelines(content)
